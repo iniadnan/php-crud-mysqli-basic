bf24e02b4b15ffdec03e19e087068e4f62067e86
Adnan Muttaqin <muh.adnan.muttaqin@gmail.com>
2023-07-05 18:46:07 +0700

    logout user

--- profile.php
+++ profile.php
@@ -62,12 +62,12 @@ if (!isset($_SESSION['email'])) {
                     </svg>                                                                                      
                     <h3 class="font-medium text-lg text-gray-700">Pusat Bantuan</h3>
                 </div>
-                <div class="flex items-center gap-x-3 bg-white py-3 px-5 text-gray-700 rounded-md">
+                <a href="./systems/users/logout.php" class="flex items-center gap-x-3 bg-white py-3 px-5 text-gray-700 rounded-md">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                     </svg>                                                                                                          
                     <h3 class="font-medium text-lg text-gray-700">Keluar</h3>
-                </div>
+                </a>
             </aside>
             <section class="w-8/12">
                 <h3 class="font-semibold text-xl text-gray-900 mb-8">Riwayat Pesanan</h3>
